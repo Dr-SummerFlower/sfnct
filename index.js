151793b920d631b6cb80b94c6dad71d676b3152f
Dr-SummerFlower <1721807170@qq.com>
2023-10-03 03:53:44 +0800

    v1.0.9

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 #!/usr/bin/env node
 
 /**
- * @文件名: index.js
- * @创建者: 夏花
- * @创建时间: 2023-09-27
+ * @File: index.js
+ * @author: 夏花
+ * @time: 2023-09-27
  */
 
 import { program } from 'commander';
 import path from 'path';
 import os from 'os';
-import { createTemplate, createFile } from './utils/create/template.js';
+import { createFile, createTemplate } from './utils/create/template.js';
 import listTemplates from './utils/list.js';
 import { execSync } from 'child_process';
-import { createRequire } from 'module'
-const requireUtil = createRequire(import.meta.url)
+import { readFileSync } from 'fs';
 
-const packageJson = requireUtil('./package.json')
+const packageJson = JSON.parse(readFileSync('./package.json'));
 
 /**
  * 显示系统信息，包括平台、主机名、系统版本、当前用户、用户主目录、字节顺序。
  */
 const systemInformation = () => {
-    const system = {
-        平台: os.platform(),
-        主机名: os.hostname(),
-        系统版本: os.release(),
-        当前用户: os.userInfo().username,
-        用户主目录: os.homedir(),
-        字节顺序: os.endianness(),
-    };
-    console.info(system);
+	const system = {
+		平台: os.platform(),
+		主机名: os.hostname(),
+		系统版本: os.release(),
+		当前用户: os.userInfo().username,
+		用户主目录: os.homedir(),
+		字节顺序: os.endianness(),
+	};
+	console.info(system);
 };
 
 program
-    .version(packageJson.version, '-v, --version', '查看版本号')
-    .helpOption('-h, --help', '查看帮助信息')
-    .option('-c, --create', '创建一个新的模版，需要配合其他参数执行')
-    .option('-t, --template <name>', '创建一个新模板,需要配合 -c 命令使用')
-    .option('-f, --file <name>', '创建一个新文件,需要配合 -c 命令使用')
-    .option('-l, --list', '列出所有可用模版')
-    .option('-o, --OS', '查看系统信息')
-    .parse(process.argv);
+	.version(packageJson.version, '-v, --version', '查看版本号')
+	.helpOption('-h, --help', '查看帮助信息')
+	.option('-c, --create', '创建一个新的模版，需要配合其他参数执行')
+	.option('-t, --template <name>', '创建一个新模板,需要配合 -c 命令使用')
+	.option('-f, --file <name>', '创建一个新文件,需要配合 -c 命令使用')
+	.option('-l, --list', '列出所有可用模版')
+	.option('-o, --OS', '查看系统信息')
+	.parse(process.argv);
 
 const env = program.opts();
 
@@ -49,34 +48,41 @@ const globalPath = execSync('npm root -g').toString().trim();
 let globalPackagePath;
 const osType = os.type();
 if (osType === 'Windows_NT') {
-    // 设置 Windows 下的路径
-    globalPackagePath = path.join(globalPath, 'sfnct');
+	// 设置 Windows 下的路径
+	globalPackagePath = path.join(globalPath, 'sfnct');
 } else if (osType === 'Darwin') {
-    // 设置 macOS 下的路径
-    globalPackagePath = path.join(globalPath, 'sfnct');
+	// 设置 macOS 下的路径
+	globalPackagePath = path.join(globalPath, 'sfnct');
 } else if (osType === 'Linux') {
-    // 设置 Linux 下的路径
-    globalPackagePath = path.join(globalPath, 'sfnct');
+	// 设置 Linux 下的路径
+	globalPackagePath = path.join(globalPath, 'sfnct');
 } else {
-    console.warn(`当前系统为${osType},该系统并不是常见系统，未经测试，如果出现错误请反馈到 github:https://github.com/Dr-SummerFlower/sfnct`)
+	// eslint和prettier规则不匹配，字符串过长导致换行，
+	// 换行导致末尾自动添加逗号，导致eslint报错
+	const msg = `当前系统为${osType},该系统并不是常见系统，未经测试，如果出现错误请反馈到 github:https://github.com/Dr-SummerFlower/sfnct`;
+	console.warn(msg);
 }
 
 const templateDir = path.join(globalPackagePath, 'template');
 const customPath = process.cwd();
 
 if (env.create) {
-    if (env.template) {
-        const name = env.template;
-        createTemplate(name, templateDir, customPath);
-    } else if (env.file) {
-        const name = env.file;
-        createFile(name, templateDir, customPath);
-    }
+	if (env.template) {
+		const name = env.template;
+		createTemplate(name, templateDir, customPath).then((r) => {
+			return r;
+		});
+	} else if (env.file) {
+		const name = env.file;
+		createFile(name, templateDir, customPath).then((r) => {
+			return r;
+		});
+	}
 } else if (env.OS) {
-    systemInformation();
+	systemInformation();
 } else if (env.list) {
-    listTemplates(templateDir);
+	listTemplates(templateDir);
 } else {
-    console.warn('请输入正确的命令');
-    program.help();
+	console.warn('请输入正确的命令');
+	program.help();
 }
--- utils/create/template.js
+++ utils/create/template.js
@@ -1,79 +1,79 @@
-import fs from "fs/promises";
-import path from "path";
+import fs from 'fs/promises';
+import path from 'path';
 
 const copy = async (source, target) => {
-    try {
-        const content = await fs.readFile(source, "utf-8");
-        await fs.writeFile(target, content);
-    } catch (err) {
-        console.error(`复制 ${source} 到 ${target} 时出错:`, err);
-    }
+	try {
+		const content = await fs.readFile(source, 'utf-8');
+		await fs.writeFile(target, content);
+	} catch (err) {
+		console.error(`复制 ${source} 到 ${target} 时出错:`, err);
+	}
 };
 
 const copyDirectory = async (source, target) => {
-    try {
-        const files = await fs.readdir(source);
+	try {
+		const files = await fs.readdir(source);
 
-        for (const file of files) {
-            const sourcePath = path.join(source, file);
-            const targetPath = path.join(target, file);
+		for (const file of files) {
+			const sourcePath = path.join(source, file);
+			const targetPath = path.join(target, file);
 
-            const stats = await fs.lstat(sourcePath);
-            if (stats.isDirectory()) {
-                await fs.mkdir(targetPath, { recursive: true });
-                await copyDirectory(sourcePath, targetPath);
-            } else {
-                await copy(sourcePath, targetPath);
-            }
-        }
-    } catch (err) {
-        console.error(`复制目录 ${source} 到 ${target} 时出错:`, err);
-    }
+			const stats = await fs.lstat(sourcePath);
+			if (stats.isDirectory()) {
+				await fs.mkdir(targetPath, { recursive: true });
+				await copyDirectory(sourcePath, targetPath);
+			} else {
+				await copy(sourcePath, targetPath);
+			}
+		}
+	} catch (err) {
+		console.error(`复制目录 ${source} 到 ${target} 时出错:`, err);
+	}
 };
 
 const createTemplate = async (name, templateDir, customPath) => {
-    try {
-        const templatePath = path.join(templateDir, name);
-        const stats = await fs.lstat(templatePath);
+	try {
+		const templatePath = path.join(templateDir, name);
+		const stats = await fs.lstat(templatePath);
 
-        if (stats.isDirectory()) {
-            await copyDirectory(templatePath, customPath);
-            console.info(`创建应用模板: ${name}`);
-        } else {
-            console.warn(`应用模板不存在: ${name}`);
-        }
-    } catch (err) {
-        console.error(`创建应用模板 ${name} 时出错:`, err);
-    }
+		if (stats.isDirectory()) {
+			await copyDirectory(templatePath, customPath);
+			console.info(`创建应用模板: ${name}`);
+		} else {
+			console.warn(`应用模板不存在: ${name}`);
+		}
+	} catch (err) {
+		console.error(`创建应用模板 ${name} 时出错:`, err);
+	}
 };
 
 const createFile = async (name, templateDir, customPath) => {
-    try {
-        const templatePath = path.join(templateDir, name);
-        const customFilePath = path.join(customPath, name);
+	try {
+		const templatePath = path.join(templateDir, name);
+		const customFilePath = path.join(customPath, name);
 
-        if (await fileExists(templatePath)) {
-            await copy(templatePath, customFilePath);
-            console.info(`创建文件模板: ${name}`);
-        } else {
-            await fs.writeFile(customFilePath, '');
-            console.info(`创建空文件: ${name}`);
-        }
-    } catch (err) {
-        console.error(`创建文件 ${name} 时出错:`, err);
-    }
+		if (await fileExists(templatePath)) {
+			await copy(templatePath, customFilePath);
+			console.info(`创建文件模板: ${name}`);
+		} else {
+			await fs.writeFile(customFilePath, '');
+			console.info(`创建空文件: ${name}`);
+		}
+	} catch (err) {
+		console.error(`创建文件 ${name} 时出错:`, err);
+	}
 };
 
 const fileExists = async (filePath) => {
-    try {
-        await fs.lstat(filePath);
-        return true;
-    } catch (err) {
-        if (err.code === 'ENOENT') {
-            return false;
-        }
-        throw err;
-    }
+	try {
+		await fs.lstat(filePath);
+		return true;
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			return false;
+		}
+		throw err;
+	}
 };
 
 export { createTemplate, createFile };
